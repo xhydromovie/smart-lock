dc99f87052388abe6d2ae477fd43fe629e8a5ec2
xhydromovie <xhydromovie@gmail.com>
2019-02-06 21:38:11 +0100

    face recognition working

--- main.py
+++ main.py
@@ -11,18 +11,48 @@ def save_photo(p):
 
 print("[LOG] Running ...")
 
-vs = VideoStream().start()
+bartek_image = face_recognition.load_image_file("bartek1.png")
+bartek_encoding = face_recognition.face_encodings(bartek_image)[0]
+
+known_face_encodings = [
+    bartek_encoding,
+]
+
+known_face_names = [
+    "bartek",
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
+
+vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0) # wait for heat camera 
 
-while True:
-    k = input("Press 'p' for photo, press 'q' for exit: ")
+input("Press Enter")
 
-    if k == 'p':
-        photo = vs.read()
-        save_photo(photo)
-    elif k == 'q':
-        break
-        
-print("Stopping camera and exit...")
+frame = vs.read()
+
+face_locations = face_recognition.face_locations(frame)
+face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+face_names = []
+
+for face_encoding in face_encodings:
+    print("Petla")
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    name = "Unknown"
+
+    if True in matches:
+        first_match_index = matches.index(True)
+        name = known_face_names[first_match_index]
+
+    face_names.append(name)
+
+if bool(face_names) == True:
+    print("Na zdjęciu znajduje się: {}".format(face_names[0]))
+else:
+    print("Nie znaleziono")
+
 vs.stop()
