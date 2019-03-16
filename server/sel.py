1e495bde485f742d50d0fda50a8a4b214412d602
xhydromovie <xhydromovie@gmail.com>
2019-03-16 22:09:50 +0100

    Delete trash files

--- server/sel.py
+++
@@ -1,6 +0,0 @@
-import wget, os
-
-url = 'http://0.0.0.0:8080/image'
-path = os.getcwd()
-
-wget.download(url, path)
--- server/uploader.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-files = {'image': open('img.jpg', 'rb')}
-r = requests.post('http://0.0.0.0:8080/entry', files=files)
-print(r.text)
